0238ff2cde866c04edb9acb81394fdf1a214f7c2
ianzin30 <leloco61@gmail.com>
2024-02-06 22:03:52 -0300

    Server connection done

---
+++ My project/Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to server");
+        PhotonNetwork.JoinLobby();
+    }
+    
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined lobby");
+        SceneManager.LoadScene("Lobby");
+    }
+}
